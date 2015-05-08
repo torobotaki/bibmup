ff5f8efbd53e084770e8fb5e8cd04c45bc91d39c
Dialekti Valsamou <dvalsamou@gmail.com>
2015-05-08 20:24:12 +0200

    added output parameter for reals

--- src/bibmup/BibLibToMindMup.java
+++ src/bibmup/BibLibToMindMup.java
@@ -4,7 +4,7 @@ public class BibLibToMindMup {
 
 		static public void main(String[] args) throws Exception {
 		
-		            if(args.length < 1 ){
+		            if(args.length < 2 ){
 		                    System.err.println("Usage: java " + BibTeXLib.class + " <Input file> <Output file>?");
 		
 		                    System.exit(-1);
@@ -12,7 +12,7 @@ public class BibLibToMindMup {
 		
 		           BibTeXLib db = new BibTeXLib(args[0], new Config());
 		           MupTree tree = new MupTree(new Config(), db);
-		           tree.writeMindMup("/tmp/out.mup");
+		           tree.writeMindMup(args[1]);
 		           System.out.println("done");
 		
 		    }
